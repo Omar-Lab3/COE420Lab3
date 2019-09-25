b79607eb0581f37c653695a98efa4fead4a9a35b
b00072756 <b00072756@aus.edu>
2019-09-25 12:42:26 +0400

    Initial Commit

---
+++ src/coe420lab3/COE420Lab3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe420lab3;
+
+/**
+ *
+ * @author b00072756
+ */
+public class COE420Lab3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+    
+}

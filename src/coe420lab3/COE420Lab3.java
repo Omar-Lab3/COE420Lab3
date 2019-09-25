c439074babb06d81af508984cadaa2a56ec78101
b00069950 <b00069950@aus.edu>
2019-09-25 12:58:16 +0400

    Feature B added

--- src/coe420lab3/COE420Lab3.java
+++ src/coe420lab3/COE420Lab3.java
@@ -10,12 +10,16 @@ package coe420lab3;
  * @author b00072756
  */
 public class COE420Lab3 {
-
+    static void addFeatureB(){
+        System.out.println("Feature B Added Successfully!");
+    }
+  
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+        addFeatureB();
     }
     
 }
